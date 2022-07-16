bf2ffbf32220a87889265a30bc2b6c9ccc167795
p4kz <wolpak@hotmail.com>
2022-07-16 05:31:27 -0300

    fix btn delete

--- src/modules/listProducts.js
+++ src/modules/listProducts.js
@@ -8,11 +8,11 @@ const createCard = (image, name, price, id) => {
 
   const createCardTemplate = `
     <div class="product__card--nav">
-      <button class="product__card--delete">
-        <svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="#ffffff" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><path d="M224,56a8,8,0,0,1-8,8h-8V208a16,16,0,0,1-16,16H64a16,16,0,0,1-16-16V64H40a8,8,0,0,1,0-16H216A8,8,0,0,1,224,56ZM88,32h80a8,8,0,0,0,0-16H88a8,8,0,0,0,0,16Z"></path></svg>
+      <button id="product__card--delete" class="btn-delete">
+        <img class="btn-delete" src="../assets/img/trash-icon.svg" />
       </button>
-      <button class="product__card--edit">
-        <svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="#ffffff" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><path d="M224,76.7,179.7,32.3a16.6,16.6,0,0,0-11.3-5A16,16,0,0,0,156.7,32L130.3,58.3h0L36.7,152A15.9,15.9,0,0,0,32,163.3V208a16,16,0,0,0,16,16H92.7a16.1,16.1,0,0,0,11.3-4.7l120-120A16.1,16.1,0,0,0,224,76.7Zm-32,32L147.3,64,168,43.3,212.7,88Z"></path></svg>
+      <button id="product__card--edit" class="btn-edit">
+        <img class="btn-edit" src="../assets/img/pen-icon.svg" />
       </button>
     </div>
     <img class="product__card--image" src=${image} />
@@ -29,7 +29,7 @@ const createCard = (image, name, price, id) => {
 const list = document.querySelector('[data-list]')
 
 list.addEventListener('click', async (e) => {
-  let deleteButton = e.target.className === 'product__card--delete'
+  let deleteButton = e.target.className === 'btn-delete'
 
   if(deleteButton) {
     try {
