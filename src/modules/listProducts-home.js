74ede668b412ceb80f95baea4402d85897de1a5f
p4kz <wolpak@hotmail.com>
2022-07-18 08:11:44 -0300

    create home page

---
+++ src/modules/listProducts-home.js
@@ -0,0 +1,31 @@
+import { service } from './service.js'
+
+const createCard = (image, name, price, id) => {
+  const createLi = document.createElement('li')
+  
+  createLi.classList.add('product__card')
+
+  const createCardTemplate = `
+    <img class="product__card--image" src=${image} />
+    <p class="product__card--name">${name}</p>
+    <p class="product__card--price">${price}</p>
+    <a class="product__card--button" href="#">Ver mais</a>
+
+  `
+
+  createLi.innerHTML = createCardTemplate
+  createLi.dataset.id = id
+  return createLi
+}
+
+const list = document.querySelector('[data-list]')
+
+const render = async () => {
+  const productList = await service.productList()
+  
+  productList.forEach(e => {
+    list.appendChild(createCard(e.image, e.name, e.price, e.id))
+  })
+}
+
+render()
