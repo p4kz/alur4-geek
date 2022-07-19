e04c7f8e71ad8ed17dfd854a66c875f4b041bb2f
p4kz <wolpak@hotmail.com>
2022-07-19 10:42:53 -0300

    fix image link form

--- src/modules/updateProduct.js
+++ src/modules/updateProduct.js
@@ -13,6 +13,7 @@ import { service } from "./service.js";
   try {
     const data = await service.setIdProduct(id)
 
+    inputImage.value= data.image
     inputName.value = data.name
     inputPrice.value = data.price
     inputDescription.value = data.description
